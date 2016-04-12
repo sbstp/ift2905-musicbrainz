c3ee8d94ca83473ed2418741ca8392b98ccb2369
Christian Doucet <alakazoo_jamazoo@hotmail.com>
2016-04-12 16:39:25 -0400

    ilar artists
    Signed-off-by: Christian Doucet <alakazoo_jamazoo@hotmail.com>

--- app/src/main/java/org/ift2905/musicbrainz/service/musicbrainz/Artist.java
+++ app/src/main/java/org/ift2905/musicbrainz/service/musicbrainz/Artist.java
@@ -3,13 +3,14 @@ package org.ift2905.musicbrainz.service.musicbrainz;
 import com.squareup.moshi.Json;
 
 import java.io.Serializable;
-import java.util.List;
 
 public class Artist implements Serializable {
 
     public String id;
 
     public String name;
+
+    public String image;
 
     @Json(name="comment")
     public String disambiguation;
--- app/src/main/java/org/ift2905/musicbrainz/similarArtists.java
+++ app/src/main/java/org/ift2905/musicbrainz/similarArtists.java
@@ -1,4 +1,4 @@
-package org.ift2905.musicbrainz.service;
+package org.ift2905.musicbrainz;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -7,79 +7,74 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.squareup.picasso.Picasso;
 
-import org.ift2905.musicbrainz.R;
+import java.io.IOException;
+import java.util.List;
+import org.ift2905.musicbrainz.service.musicbrainz.Artist;
 import org.ift2905.musicbrainz.service.musicmap.MusicMapService;
 import org.ift2905.musicbrainz.service.musicmap.MusicMapServiceError;
 
-import static org.ift2905.musicbrainz.R.id.sim_artist_name;
+import static org.ift2905.musicbrainz.R.layout.activity_similar_artists;
 
-public class similarArtists extends AppCompatActivity {
+public class similarArtists extends AppCompatActivity{
 
-    ListView list;
-    Adapter myadapter;
-    List<String> artists;
+    private ListView list;
+    private MyAdapter adapter;
+    private LayoutInflater inflater;
+    private Artist artist;
+    private List<String> similarArtistList;
+    private ImageView imageArtist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_similar_artists);
-
-        TextView tv =(TextView) findViewById((sim_artist_name));
-        ImageView im = (ImageView)findViewById(R.id.sim_artist_image);
-
+        setContentView(activity_similar_artists);
         Intent intent = getIntent();
-        String artistessimilaires = intent.getStringExtra("ARTISTESSIMILAIRES");
-
-        list = (ListView) findViewById(R.id.similarArtistsList);
+        artist = (Artist) intent.getSerializableExtra("artist");
     }
 
-   public class RunSimArtist extends AsyncTask implements AdapterView.OnItemClickListener {
+    public class RunSimArtist extends AsyncTask<String, Void, List<String>> {
 
        @Override
-       public void onPostExecute(List<MusicMapService>){
-           super.onPostExecute();
-           myadapter = new MyAdapter();
-           list.setAdapter(myadapter);
-           list.setOnItemClickListener(this);
-
+       protected void onPostExecute(List<String> strings) {
+           super.onPostExecute(strings);
+           adapter = new MyAdapter((List<Artist>) artist, inflater);//manque nom artiste(s)? en paramètre
+           list.setAdapter(adapter);
        }
 
        @Override
-       protected Object doInBackground(Object[] params) {
-           return null;
-       }
+       protected List<String> doInBackground(String... params) {
+           String artistToSearch = artist.toString();
+           MusicMapService musicMap = new MusicMapService();
 
-       @Override
-       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-           Intent intent = new Intent(this, cd_listing.class);
-           intent.putExtra("NOMARTISTE", "Artistes");
-           startActivity(intent);
-
+           try {
+               return similarArtistList = musicMap.getSimilarArtists(artistToSearch);
+           } catch (IOException e) {
+               return null;
+           } catch (MusicMapServiceError musicMapServiceError) {
+               return null;
+           }
        }
    }
 
-    public class MyAdapter extends BaseAdapter{
+    public class MyAdapter extends BaseAdapter implements View.OnClickListener {
         private LayoutInflater inflater;
+        private List<Artist> artistsA;
 
-        public MyAdapter(List<String> artists, LayoutInflater inflater){
-            this.inflater = inflater;
-            //this.artists = artists;
+        public MyAdapter(List<Artist> artistsA, LayoutInflater inflater){
+            inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+            this.artistsA = artistsA;
         }
 
         @Override
         public int getCount() {
-            return artists.size();
+            return artistsA.size();
         }
 
         @Override
@@ -94,11 +89,28 @@ public class similarArtists extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View v = inflater.inflate(R.layout.sim_artist_im, parent, false);
+            View v = convertView;
+            if (v==null) {
+                v = inflater.inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+            }
+            TextView tv = (TextView)v.findViewById(android.R.id.text1);
+            tv.setOnClickListener(this);
 
+            ImageView im = (ImageView)v.findViewById(R.id.imageArtist);
+            Picasso.with(getApplicationContext()).
+                    load(artist.image)
+                    .placeholder(R.drawable.release_group_placeholder)
+                    .into(im);
+            String similaire = similarArtistList.get(position);
+            tv.setText(similaire);
+            return  v;
+        }
 
-            tv.setText("Artistes similaires à: " );
-            return v;
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("artist", artist);
+            startActivity(intent);
         }
     }
 
